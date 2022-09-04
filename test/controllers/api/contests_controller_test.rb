b7ec4eec712a01170b4937a463c54783eac809a1
Ambika Patidar <ambika.patidar.cse@gmail.com>
2022-09-04 23:41:24 +0530

    Added test cases for contests

--- test/controllers/api/contests_controller_test.rb
+++ test/controllers/api/contests_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class Api::ContestsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'create contest' do
+    post '/api/contests', params: {
+      name: 'contest2',
+      description: 'abc',
+      platform: 'abc',
+      registration: 'abc',
+      timings: '10:00'
+    }
+    body = JSON.parse(response.body)
+    assert_equal 200, response.status
+    assert_equal 'SUCCESS', body['status']
+    assert_equal 'Contest is saved', body['message']
+  end
+
+  test 'should not contest without name' do
+    post '/api/contests', params: {
+      description: 'abc',
+      platform: 'abc',
+      registration: 'abc',
+      timings: '10:00'
+    }
+    body = JSON.parse(response.body)
+    assert_not_equal 200, response.status
+    assert_equal 'Error', body['status']
+    assert_equal 'Contest is not saved', body['message']
+  end
+
+  test 'fetch all contests' do
+    Contest.create(
+      name: 'contest2',
+      description: 'abc',
+      platform: 'abc',
+      registration: 'abc',
+      timings: '10:00'
+    )
+    get '/api/contests'
+    body = JSON.parse(response.body)
+    assert_equal 200, response.status
+    assert_equal 'SUCCESS', body['status']
+    assert_equal 'Loaded contests', body['message']
+  end
 end
--- test/models/contest_test.rb
+++ test/models/contest_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ContestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'the truth' do
+    assert true
+  end
+
+  test 'should not save contest without name, registration and timings' do
+    contest = Contest.new(description: 'test', platform: 'test')
+    assert_not contest.save
+  end
 end
